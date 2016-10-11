7764574b8a32efb57571fb2da44e7e38b37d3796
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2016-10-12 01:56:59 +0300

    Added 'authManager' in common config

--- backend/config/main.php
+++ backend/config/main.php
@@ -1,5 +1,4 @@
 <?php
-use bl\cms\shop\backend\components\CreateImageImagine;
 
 $params = array_merge(
     require(__DIR__ . '/../../common/config/params.php'),
@@ -85,102 +84,6 @@ return [
             'showScriptName' => false,
             'rules' => [
             ],
-        ],
-        'shop_imagable' => [
-            'class' => 'bl\imagable\Imagable',
-            'imageClass' => CreateImageImagine::className(),
-            'nameClass' => 'bl\imagable\name\CRC32Name',
-            'imagesPath' => '@frontend/web/images',
-            'categories' => [
-                'origin' => false,
-                'category' => [
-                    'shop-product' => [
-                        'origin' => false,
-                        'size' => [
-                            'big' => [
-                                'width' => 1500,
-                                'height' => 500
-                            ],
-                            'thumb' => [
-                                'width' => 500,
-                                'height' => 500,
-                            ],
-                            'small' => [
-                                'width' => 150,
-                                'height' => 150
-                            ]
-                        ]
-                    ],
-                    'shop-vendors' => [
-                        'origin' => false,
-                        'size' => [
-                            'big' => [
-                                'width' => 1500,
-                                'height' => 500
-                            ],
-                            'thumb' => [
-                                'width' => 320,
-                                'height' => 240,
-                            ],
-                            'small' => [
-                                'width' => 150,
-                                'height' => 150
-                            ]
-                        ]
-                    ],
-                    'cover' => [
-                        'origin' => false,
-                        'size' => [
-                            'big' => [
-                                'width' => 1500,
-                                'height' => 500
-                            ],
-                            'thumb' => [
-                                'width' => 500,
-                                'height' => 500,
-                            ],
-                            'small' => [
-                                'width' => 150,
-                                'height' => 150
-                            ]
-                        ]
-                    ],
-                    'thumbnail' => [
-                        'origin' => false,
-                        'size' => [
-                            'big' => [
-                                'width' => 1500,
-                                'height' => 500
-                            ],
-                            'thumb' => [
-                                'width' => 500,
-                                'height' => 500,
-                            ],
-                            'small' => [
-                                'width' => 150,
-                                'height' => 150
-                            ]
-                        ]
-                    ],
-                    'menu_item' => [
-                        'origin' => false,
-                        'size' => [
-                            'big' => [
-                                'width' => 1500,
-                                'height' => 500
-                            ],
-                            'thumb' => [
-                                'width' => 500,
-                                'height' => 500,
-                            ],
-                            'small' => [
-                                'width' => 150,
-                                'height' => 150
-                            ]
-                        ]
-                    ]
-                ]
-            ]
         ],
     ],
     'params' => $params,
